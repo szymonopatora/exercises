958bef83c3c720b5433b711429a1c0420be49a9f
Szymon Patora <szymonopatora@gmail.com>
2019-02-20 12:21:25 +0100

    Exercises on efficiency of the runtime of concatenation, StringBuilder and StringBuffer

---
+++ src/main/java/ConcatenationVsStringBuilderVsStringBuffer.java
@@ -0,0 +1,49 @@
+public class ConcatenationVsStringBuilderVsStringBuffer {
+    /**
+     * Concatenation vs StringBuilder vs StringBuffer
+     *
+     * @author Szymon Patora [http://github.com/szymonopatora]
+     * <p>
+     * Exercise to present the difference in efficiency between the mentioned methods.
+     */
+
+    public static void main(String[] args) {
+
+        final int MILLIS = 1_000_000;
+
+        String stringExample = "";
+        long startString = System.currentTimeMillis();
+        for (int i = 0; i < MILLIS; i++) {
+            stringExample += "a";
+        }
+        long stopString = System.currentTimeMillis();
+
+        long resultString = stopString - startString;
+        System.out.println("Runtime for String concatenation: " + resultString+" ms");
+
+        System.out.println("***");
+
+        StringBuilder stringBuilderExample = new StringBuilder();
+        long startStringBuilder = System.currentTimeMillis();
+        for (int i = 0; i < MILLIS; i++) {
+            stringBuilderExample.append("a");
+        }
+        long stopStringBuilder = System.currentTimeMillis();
+
+        long resultStringBuilder = stopStringBuilder - startStringBuilder;
+        System.out.println("Runtime for StringBuilder: " + resultStringBuilder+" ms");
+
+        System.out.println("***");
+
+        StringBuilder stringBufferExample = new StringBuilder();
+        long startStringBuffer = System.currentTimeMillis();
+        for (int i = 0; i < MILLIS; i++) {
+            stringBufferExample.append("a");
+        }
+        long stopStringBuffer = System.currentTimeMillis();
+
+        long resultStringBuffer = stopStringBuffer - startStringBuffer;
+        System.out.println("Runtime for StringBuilder: " + resultStringBuffer+" ms");
+    }
+
+}
